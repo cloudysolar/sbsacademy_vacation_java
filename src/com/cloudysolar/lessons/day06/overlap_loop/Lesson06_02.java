3c7a9a6bca9e0f4535cf89e00066bd3204c58c79
cloudysolar <cloudysolar@naver.com>
2021-08-12 19:09:00 +0900

    6강 소스코드 수정

--- src/com/cloudysolar/lessons/day06/overlap_loop/Lesson06_02.java
+++ src/com/cloudysolar/lessons/day06/overlap_loop/Lesson06_02.java
@@ -6,11 +6,11 @@ public class Lesson06_02 {
 		int level = 3;
 
 		for (int i = 0; i < level; i++) {
-			for (int j = level; j > i; j--) {
+			for (int j = (level - 1); j > i; j--) {
 				System.out.print(" ");
 			}
 
-			for (int k = 0; k <= (i * (level - 1)); k++) {
+			for (int k = 0; k <= (i * 2); k++) {
 				System.out.print("*");
 			}
 
